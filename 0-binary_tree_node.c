0c753aedd9ab5e94779e507c769a759f3c8c05c3
dEstInY-de-Gold <noahayebu@gmail.com>
2023-05-31 15:51:03 +0000

    create trees node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - A function for creating a binary tree.
+ * @parent: A pointer to parent node.
+ * @value: Data to store.
+ *
+ * Return: A pointer to root node.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *ptr;
+
+	ptr = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (ptr == NULL)
+	{
+		return NULL;
+	}
+
+	ptr->n = value;
+	ptr->parent = parent;
+	ptr->left = NULL;
+	ptr->right = NULL;
+
+	return (ptr);
+}
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Insertion at left node.
+ * @parent: parent node.
+ * @value: Data for storing.
+ *
+ * Return: Pointer to created node.
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *ptr, *tmp;
+
+	/*
+	ptr = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (ptr == NULL)
+	{
+		return NULL;
+	} */
+
+	ptr = binary_tree_node(parent, value);
+
+	if (parent->left != NULL)
+	{
+		/* ptr = binary_tree_node(parent, value); */
+		tmp = parent->left;
+		/* parent->left->n = value; */
+		parent->left = ptr;
+		ptr->left = tmp;
+		tmp->parent = parent;
+	}
+	else
+	{
+		/* ptr = binary_tree_node(parent, value); */
+		parent->left = ptr;
+		ptr->parent = parent;
+	}
+	return (ptr->left);
+}
