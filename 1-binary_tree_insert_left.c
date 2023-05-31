cbaea833481b0af0be96865298c1fc11825f88c9
Noah <noahayebu@gmail.com>
2023-05-31 16:24:47 +0000

    adding a node to left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	ptr = binary_tree_node(parent, value);
 
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 	if (parent->left != NULL)
 	{
 		/* ptr = binary_tree_node(parent, value); */
