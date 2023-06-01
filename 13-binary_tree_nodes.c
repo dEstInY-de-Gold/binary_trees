93a345c54156c513fabdb21b1bdc3f5b1f730369
Noah <noahayebu@gmail.com>
2023-06-01 20:57:11 +0000

    count parent nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,15 +11,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t cnt, l_cnt, r_cnt;
 
-	if (!(tree))
+	if ((!(tree)) || ((!(tree->left)) && (!(tree->right))))
 		return (0);
-	else if (tree->left || tree->right)
+	else if ((tree->left) || (tree->right))
 		return (1);
-	else
-	{
-		l_cnt = binary_tree_nodes(tree->left);
-		r_cnt = binary_tree_nodes(tree->right);
-		cnt = l_cnt + r_cnt;
-	}
+
+	l_cnt = binary_tree_nodes(tree->left);
+	r_cnt = binary_tree_nodes(tree->right);
+	cnt = 1 + l_cnt + r_cnt;
+
 	return (cnt);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,9 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
