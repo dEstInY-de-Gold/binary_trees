94006fcd22ced7baf533a0993acb623735381faf
Noah <noahayebu@gmail.com>
2023-06-03 11:40:41 +0000

    level order

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,6 +7,37 @@
  */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
-
 {
+	const binary_tree_t **queue, *crnt;
+	int front, rear;
+
+	if (tree == NULL)
+		return;
+	queue = (const binary_tree_t **)malloc(sizeof(binary_tree_t *));
+	if (queue == NULL)
+		return;
+	front = rear = 0;
+	queue[rear] = tree;
+	rear++;
+	while (front < rear)
+	{
+		crnt = queue[front];
+		front++;
+		func(crnt->n);
+		if (crnt->left)
+		{
+			queue = (const binary_tree_t **)realloc(queue,
+					(rear + 1) * sizeof(binary_tree_t *));
+			queue[rear] = crnt->left;
+			rear++;
+		}
+		if (crnt->right)
+		{
+			queue = (const binary_tree_t **)realloc(queue,
+					(rear + 1) * sizeof(binary_tree_t *));
+			queue[rear] = crnt->right;
+			rear++;
+		}
+	}
+	free(queue);
 }
